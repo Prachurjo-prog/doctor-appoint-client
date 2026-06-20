3833373dca731c65b525afe5245dc97cd7b1a66d
noob-prachurjo <prachurjobarmon2008@gmail.com>
2026-06-20 13:43:37 +0600

    HospitalServices completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 import Hero from "@/Components/Hero";
+import HospitalServices from "@/Components/HospitalServices";
+import TopRatedDoctors from "@/Components/TopRatedDoctor";
 
 
 export default function Home() {
   return (
     <div>
       <Hero/>
+      <TopRatedDoctors/>
+      <HospitalServices/>
     </div>
   );
 }
